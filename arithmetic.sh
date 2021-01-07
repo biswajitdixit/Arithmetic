172023a7ffe79483c1ee7e6a4aec9c73cead819e
biswajitdixit <buntyoec1998@gmail.com>
2021-01-07 20:29:09 +0530

    ADDED:arithmetic operation 2

--- arithmetic.sh
+++ arithmetic.sh
@@ -6,7 +6,7 @@ read -p "Enter a:" a
 read -p "Enter b:" b
 read -p "Enter c:" c
 
-compute1=$(( $a + $b * $c ))
+compute2=$(( $a * $b + $c ))
 
-echo " compute1:$compute1 "
+echo " compute2:$compute2 "
 
